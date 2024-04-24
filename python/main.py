d79456c1e859b05a0c7ed4d729418d2dba7f8b37
bjhickman <bjhickman@spscommerce.com>
2024-04-24 00:12:39 -0500

    main changes

--- python/main.py
+++ python/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI, HTTPException
 from typing import Dict, Any
+
+from starlette.responses import JSONResponse
+
 from send_email import send_email
 
 app = FastAPI()
 
 @app.post("/send_email")
-async def send_email_endpoint(request: Dict[str, Any]) -> Dict[str, str]:
+async def send_email_endpoint(request: Dict[str, Any]) -> JSONResponse:
     try:
         return await send_email(request, None)
     except Exception as e:
